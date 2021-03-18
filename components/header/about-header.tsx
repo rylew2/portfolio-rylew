366f2767dae1dd41aec8cd5c6f44ebb66232e260
Ryan Lewis <r2c2ryan@gmail.com>
2021-03-17 23:42:03 -0700

    update avatar, and header

--- components/header/about-header.tsx
+++ components/header/about-header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { Container } from "../container";
 import { StyledPageHeading } from "../styles/header.styles";
 
@@ -7,7 +6,7 @@ const AboutHeader = () => (
   <StyledPageHeading>
     <Container>
       <div className="header-container">
-        <h1 className="about-header">About Me 🧘🏾‍♂️</h1>
+        <h1 className="about-header">About Me</h1>
       </div>
     </Container>
   </StyledPageHeading>
